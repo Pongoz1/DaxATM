a51d8696bd52713373a2fdefd4bbe535895071f4
Pongoz1 <132121751+Pongoz1@users.noreply.github.com>
2026-05-16 22:49:18 +0700

    update : memperbarui tombol pada cardless access

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,7 @@ class DaxAtmScreen extends StatelessWidget{
                   ////////////////////////////////////////////////////////////DALAM BOX///////////////////////////////////////
                   const SizedBox(height: 50),
                   Container( 
-                    width: 540,
+                    width: 700,
                     padding: const EdgeInsets.fromLTRB(24, 28, 24, 28),
                     decoration: BoxDecoration(
                       color: Colors.white.withOpacity(0.25),
@@ -102,18 +102,79 @@ class DaxAtmScreen extends StatelessWidget{
                         Row ( 
                           children: [
                             Expanded( 
-                              child : _buildAccessButton ( 
-                                icons : Icons.face_retouching_natural,
-                                label : 'FACE ID',
-                                onTap : (){},
-                              ),
-                            ),                                              
+                              child: SizedBox(
+                    width : 360,
+                    height : 120,
+                    child : ElevatedButton (  
+                      style : ElevatedButton.styleFrom  ( 
+                        backgroundColor : Colors.white,
+                        foregroundColor : Colors.black, 
+                        shape: RoundedRectangleBorder( 
+                          borderRadius: BorderRadius.circular(0)
+                        ),
+                        elevation : 8,
+
+                      ),
+                      onPressed: () {},
+                      child: Column(  
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const[
+                          Icon(
+                            Icons.face_retouching_natural,
+                            size: 42,
+                          ),
+                          SizedBox(height: 10),
+                          Text (  
+                            'FACE ID',    
+                            style : TextStyle( 
+                              fontSize : 22,
+                              fontWeight : FontWeight.w700,
+                              letterSpacing : 1,
+                            ),
+                          ),
+
+                        ],
+                      ),
+                    ),
+                  ),
+                            ),
+                            const SizedBox(width: 40),
                             Expanded(  
-                              child : _buildAccessButton ( 
-                                icons: Icons.fingerprint,
-                                label: 'FINGERPRINT',
-                                onTap: (){},                
-                              ),
+                              child: SizedBox( 
+                    width : 360,
+                    height : 120,
+                    child : ElevatedButton (  
+                      style : ElevatedButton.styleFrom  ( 
+                        backgroundColor : Colors.white,
+                        foregroundColor : Colors.black, 
+                        shape: RoundedRectangleBorder( 
+                          borderRadius: BorderRadius.circular(0)
+                        ),
+                        elevation : 8,
+
+                      ),
+                      onPressed: () {},
+                      child: Column(  
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const[
+                          Icon(
+                            Icons.fingerprint,
+                            size: 42,
+                          ),
+                          SizedBox(height: 10),
+                          Text (  
+                            'FINGERPRINT',    
+                            style : TextStyle( 
+                              fontSize : 22,
+                              fontWeight : FontWeight.w700,
+                              letterSpacing : 1,
+                            ),
+                          ),
+
+                        ],
+                      ),
+                    ),
+                  ),
                             ),
                           ],
                         ),                     
